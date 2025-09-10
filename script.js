ba35cc54cfd901353547b6e94f8ff99e97acc0c7
Colton Phass <coltonsphass@gmail.com>
2025-09-10 14:02:05 -0400

    added animations for resume

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ open.addEventListener('click', () => navContainer.classList.add('show-nav'));
 close.addEventListener('click', () =>
   navContainer.classList.remove('show-nav')
 );
+
+const jobs = document.querySelectorAll('.job');
+
+window.addEventListener('scroll', checkJobs);
+
+checkJobs();
+
+function checkJobs() {
+  const triggerBottom = window.innerHeight * 0.8;
+
+  jobs.forEach((job) => {
+    const jobTop = job.getBoundingClientRect().top;
+    console.log(job.getBoundingClientRect().top);
+
+    if (jobTop < triggerBottom) {
+      job.classList.add('show');
+    } else {
+      job.classList.remove('show');
+    }
+  });
+}
