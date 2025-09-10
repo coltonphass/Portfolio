ea3f190903c698e69f97546e83fccf8f47df4b41
Colton Phass <coltonsphass@gmail.com>
2025-09-10 18:06:50 -0400

    My animations for resume were breaking on mobile no matter what i did, so i added a mobile checker function in js and force the opacity and animations to not happen on mobile resume site.

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const navContainer = document.getElementById('nav-container');
+document.addEventListener('DOMContentLoaded', () => {
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const navContainer = document.getElementById('nav-container');
+  const jobs = document.querySelectorAll('.job');
 
-open.addEventListener('click', () => navContainer.classList.add('show-nav'));
-close.addEventListener('click', () =>
-  navContainer.classList.remove('show-nav')
-);
+  open.addEventListener('click', () => navContainer.classList.add('show-nav'));
+  close.addEventListener('click', () =>
+    navContainer.classList.remove('show-nav')
+  );
 
-const jobs = document.querySelectorAll('.job');
+  const isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return (
+        navigator.userAgent.match(/IEMobile/i) ||
+        navigator.userAgent.match(/WPDesktop/i)
+      );
+    },
+    any: function () {
+      return (
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Windows()
+      );
+    },
+  };
+  window.addEventListener('scroll', checkJobs);
+  checkJobs();
 
-window.addEventListener('scroll', checkJobs);
+  function checkJobs() {
+    const triggerBottom = window.innerHeight * 0.8;
+    jobs.forEach((job) => {
+      if (isMobile.any()) {
+        // Remove transforms for mobile
+        job.style.transform = 'none';
+        job.style.opacity = '1';
+      } else {
+        const jobTop = job.getBoundingClientRect().top;
 
-checkJobs();
-
-function checkJobs() {
-  const triggerBottom = window.innerHeight * 0.8;
-
-  jobs.forEach((job) => {
-    const jobTop = job.getBoundingClientRect().top;
-    console.log(job.getBoundingClientRect().top);
-
-    if (jobTop < triggerBottom) {
-      job.classList.add('show');
-    } else {
-      job.classList.remove('show');
-    }
-  });
-}
+        if (jobTop < triggerBottom) {
+          job.classList.add('show');
+        } else {
+          job.classList.remove('show');
+        }
+      }
+    });
+  }
+});
